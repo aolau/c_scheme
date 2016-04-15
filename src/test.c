5c8d5476c759789692cb926fdd9b08d45273bf4f
Andreas Olausson <olausson.andreas@gmail.com>
2016-04-15 12:08:32 +0200

    Test case info when fail

--- src/test.c
+++ src/test.c
@@ -12,21 +12,27 @@ static void test_success() {
     test_progress(".");
 }
 
-static void test_error() {
+static void test_error(const char *file, int line,
+                       const char *a_str, const char *b_str) {
     test_error_count++;
     test_progress("e");
+    printf("\nAssert failed in %s on line %d:\n", file, line);
+    printf("%s\n", a_str);
+    printf("%s\n", b_str);
 }
 
-static void test(int cond) {
+static void test_assert(int cond, const char *file, int line,
+                        const char *a_str, const char *b_str) {
     if (cond) {
         test_success();
     } else {
-        test_error();
+        test_error(file, line, a_str, b_str);
     }
 }
     
-#define TEST_EQ(a, b) test((a) == (b))
-#define TEST_EQ_STR(a, b) test( strcmp((a), (b)) == 0)
+#define TEST_EQ(a, b) test_assert((a) == (b), __FILE__, __LINE__, #a, #b)
+#define TEST_EQ_STR(a, b) test_assert( strcmp((a), (b)) == 0,\
+                                       __FILE__, __LINE__, #a, #b)
 
     
 #define TEST_BEGIN(name)\
@@ -40,9 +46,10 @@ static void test(int cond) {
 
 TEST_BEGIN("scheme");
 TEST_EQ(1, 1);
-
+TEST_EQ(1, 0);
 TEST_EQ(0, 0);
 TEST_EQ_STR("hej", "hej");
+TEST_EQ_STR("hej", "nej");
 
 TEST_END;
 
